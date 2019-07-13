c91e297d8894d5019506605017d4b385a1824d71
mike-s-o <mike.solcielo@gmail.com>
2019-07-13 20:08:28 +0100

    Updates

---
+++ precache-manifest.99ee39c83b188f3770826e4e24a80e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff84ac3304f2451350c4b0bb5064295",
+    "url": "/kaleidos/index.html"
+  },
+  {
+    "revision": "3a5bff8a9372fa59d7f9",
+    "url": "/kaleidos/static/css/main.b4ce7b8d.chunk.css"
+  },
+  {
+    "revision": "bec8ef11901d16ffca29",
+    "url": "/kaleidos/static/js/2.62c03a3f.chunk.js"
+  },
+  {
+    "revision": "3a5bff8a9372fa59d7f9",
+    "url": "/kaleidos/static/js/main.b0cb6228.chunk.js"
+  },
+  {
+    "revision": "a57334b776417a3b8af8",
+    "url": "/kaleidos/static/js/runtime~main.b950cada.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaleidos/static/media/logo.5d5d9eef.svg"
+  }
+]);
